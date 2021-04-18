ccc4bda2d1d5e1d4dd2b291f472ea6336ee14897
Alex Drozd <drozdster@gmail.com>
2021-04-18 16:38:45 +0200

    trying some module mocking

--- simple_mockforce/biz_logic.py
+++ simple_mockforce/biz_logic.py
@@ -1,9 +1,7 @@
-from simple_mockforce import mock_salesforce
 from simple_salesforce import Salesforce
 
 
-@mock_salesforce
-def test_sf_mock():
+def run():
     salesforce = Salesforce(
         username="hi", password="hello", security_token="123", domain="mock"
     )
---
+++ simple_mockforce/salesforce.py
@@ -0,0 +1,2 @@
+class Salesforce:
+    pass
