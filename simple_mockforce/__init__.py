29cdf22337807e675abf31cc55deb01ab6d6fa17
Alex Drozd <drozdster@gmail.com>
2021-04-20 13:35:09 +0200

    switching to response callbacks

--- simple_mockforce/__init__.py
+++ simple_mockforce/__init__.py
@@ -1,12 +1,38 @@
+import json
+import re
+
+import responses
+
 from simple_mockforce.mock import MockSalesforce as Salesforce
+from simple_mockforce.patch_constants import (
+    LOGIN_URL,
+    SOAP_API_LOGIN_RESPONSE,
+    BASE_URL,
+    QUERY_URL,
+)
 
 
 def mock_salesforce(func):
+    @responses.activate
     def wrapper(*args, **kwargs):
-        import sys
+        responses.add(
+            responses.POST,
+            re.compile(LOGIN_URL),
+            body=SOAP_API_LOGIN_RESPONSE,
+            content_type="text/xml",
+        )
 
-        sys.modules["simple_salesforce"] = __import__(__name__)
+        def request_callback(request):
+            resp_body = {}
+            headers = {}
+            return (200, headers, json.dumps(resp_body))
+
+        responses.add_callback(
+            responses.GET,
+            re.compile(QUERY_URL),
+            callback=request_callback,
+            content_type="content/json",
+        )
         func(*args, **kwargs)
-        del sys.modules["simple_salesforce"]
 
     return wrapper
--- tests/test_simple_mockforce.py
+++ tests/test_simple_mockforce.py
@@ -1,10 +1,9 @@
 from simple_mockforce import mock_salesforce
+from simple_salesforce import Salesforce
 
 
 @mock_salesforce
 def test_sf_mock():
-    from simple_salesforce import Salesforce
-
     salesforce = Salesforce(
         username="hi", password="hello", security_token="123", domain="mock"
     )
