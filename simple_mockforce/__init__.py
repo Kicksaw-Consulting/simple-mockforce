dba32479eb98659ec0706bdb73b9a731c9b55301
Alex Drozd <drozdster@gmail.com>
2021-04-20 13:17:26 +0200

    simplifying directory structure

--- simple_mockforce/__init__.py
+++ simple_mockforce/__init__.py
@@ -1,3 +1,12 @@
-from simple_mockforce.core import mock_salesforce
-
 from simple_mockforce.mock import MockSalesforce as Salesforce
+
+
+def mock_salesforce(func):
+    def wrapper(*args, **kwargs):
+        import sys
+
+        sys.modules["simple_salesforce"] = __import__(__name__)
+        func(*args, **kwargs)
+        del sys.modules["simple_salesforce"]
+
+    return wrapper
--- simple_mockforce/core.py
+++
@@ -1,9 +0,0 @@
-def mock_salesforce(func):
-    def wrapper(*args, **kwargs):
-        import sys
-
-        sys.modules["simple_salesforce"] = __import__("simple_mockforce")
-        func(*args, **kwargs)
-        del sys.modules["simple_salesforce"]
-
-    return wrapper
