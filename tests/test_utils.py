35c75c35dab654cce50ec5377729f0ce747fa820
Alex Drozd <drozdster@gmail.com>
2021-04-22 11:30:45 +0200

    adding a few more utils tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,10 @@
 from simple_mockforce.utils import (
     parse_detail_url,
     parse_create_url,
+    parse_job_batch_url,
+    parse_batch_result_url,
     find_object_and_index,
+    parse_batch_detail_url,
 )
 
 
@@ -28,6 +31,30 @@ def test_parse_create_url():
     sobject = parse_create_url("/services/data/v42.0/sobjects/Contact/")
 
     assert sobject == "Contact"
+
+
+def test_parse_job_batch_url():
+    job_id = parse_job_batch_url("/services/async/42.0/job/123456/batch")
+
+    assert job_id == "123456"
+
+
+def test_parse_batch_detail_url():
+    job_id, batch_id = parse_batch_detail_url(
+        "/services/async/42.0/job/123456/batch/123"
+    )
+
+    assert job_id == "123456"
+    assert batch_id == "123"
+
+
+def test_parse_batch_result_url():
+    job_id, batch_id = parse_batch_result_url(
+        "/services/async/42.0/job/123456/batch/123/result"
+    )
+
+    assert job_id == "123456"
+    assert batch_id == "123"
 
 
 def test_find_object_and_index():
