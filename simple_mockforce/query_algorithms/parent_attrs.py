345d93bc913154712b35d0c7fee0ff6f344a75e5
Alex Drozd <drozdster@gmail.com>
2022-07-08 09:46:19 -0500

    Modification to add_parent_object_attributes

--- simple_mockforce/query_algorithms/parent_attrs.py
+++ simple_mockforce/query_algorithms/parent_attrs.py
@@ -6,9 +6,15 @@ def add_parent_object_attributes(
         normalized_parent_sobject_name = (
             virtual_salesforce._related_object_name_to_object_name(parent_sobject_name)
         )
+        # for some standard lookup fields, we need to append 'Id'
+        lookup_field_name = (
+            normalized_parent_sobject_name
+            if normalized_parent_sobject_name in sobject
+            else f"{normalized_parent_sobject_name}Id"
+        )
         parent_object = virtual_salesforce.get(
             normalized_parent_sobject_name,
-            sobject[normalized_parent_sobject_name],
+            sobject[lookup_field_name],
         )
         if parent_sobject_name not in record:
             record[parent_sobject_name] = dict()
